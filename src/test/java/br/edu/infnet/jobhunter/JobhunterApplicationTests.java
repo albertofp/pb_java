e0398b22d76b29f6d5bb6494c758428bbb3f9394
albertofp <albertopluecker@gmail.com>
2023-11-23 22:56:01 +0100

    init

---
+++ src/test/java/br/edu/infnet/jobhunter/JobhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.jobhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
